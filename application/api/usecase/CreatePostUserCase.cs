d82b167a00ffaf2e8675e84db5713e533a9634b5
danielandersonBC96 <dev.danielanderson@gmail.com>
2025-11-03 13:35:05 -0400

    criando camada de  usece case do post e do comment

--- application/api/usecase/CreatePostUserCase.cs
+++ application/api/usecase/CreatePostUserCase.cs
@@ -1,60 +1,38 @@
-﻿using API_de_rede_social.application.usecases;
-using API_de_rede_social.application.usecases.posts;
-using API_de_rede_social.domain.entities;
+﻿using API_de_rede_social.domain.entities;
 using API_de_rede_social.Domain.Repository;
-using Microsoft.AspNetCore.Http.HttpResults;
+using System;
+using System.Threading.Tasks;
 
-namespace API_de_rede_social.application.api.usecase
+namespace API_de_rede_social.application.usecases.posts
 {
-    public class CreatePostUserCase : ICreatePostUseCase
+    public class CreatePostUseCase : ICreatePostUseCase
     {
-        public readonly IPostRepository postRepository;
-        public readonly IUserRepository userRepository;
-    
-        public CreatePostUserCase(IPostRepository postRepository, IUserRepository userRepository)
-        {
-            this.postRepository = postRepository;
-            this.userRepository = userRepository;
+        private readonly IPostRepository _postRepository;
+        private readonly IUserRepository _userRepository;
 
+        public CreatePostUseCase(IPostRepository postRepository, IUserRepository userRepository)
+        {
+            _postRepository = postRepository;
+            _userRepository = userRepository;
         }
 
-        public Task AddAsync(CommentEntities comment)
+        public async Task ExecuteAsync(Guid userId, string content)
         {
-            throw new NotImplementedException();
+            // Verifica se o usuário existe
+            var user = await _userRepository.GetByIdAsync(userId);
+            if (user == null)
+                throw new Exception("Usuário não encontrado.");
+
+            // Cria o post
+            var post = new PostEntities
+            {
+                UserId = userId,
+                Content = content,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            // Salva no repositório
+            await _postRepository.AddAsync(post);
         }
-
-        public Task ExecuteAsync(Guid userId, Guid postId, string content)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ExecuteAsync(Guid userId, string content)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task ExecutyAsync(string username, string password)
-        {
-
-            //Valida se o usuario existe
-
-            var name = await userRepository.GetAllAsync();
-            if (name == null)
-                {
-                    Id = Guid.NewGuid();
-                    Name = name;
-                    Email = email;
-                    CreatedAt = DateTime.UtcNow;
-
-            }
-
-            await postRepository.AddAsync(post);
-        }
-
-
-
-
     }
-
-
-  }
+}
---
+++ application/api/usecase/UpdatePostUseCase.cs
@@ -0,0 +1,31 @@
+﻿
+using API_de_rede_social.Domain.Repository;
+
+
+
+namespace API_de_rede_social.application.usecases.posts
+{
+    public class UpdatePostUseCase : IUpdatePostUseCase
+    {
+        private readonly IPostRepository _postRepository;
+
+        public UpdatePostUseCase(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task ExecuteAsync(Guid postId, string newContent)
+        {
+            // Busca o post
+            var post = await _postRepository.GetByIdAsync(postId);
+            if (post == null)
+                throw new Exception("Post não encontrado.");
+
+            // Atualiza o conteúdo
+            post.Content = newContent;
+
+            // Salva no banco
+            await _postRepository.UpdateAsync(post);
+        }
+    }
+}
