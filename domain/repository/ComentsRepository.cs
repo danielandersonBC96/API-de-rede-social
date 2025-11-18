11dee6f154ef68b9b0f5c40df51ff4d80f9db08d
danielandersonBC96 <dev.danielanderson@gmail.com>
2025-11-18 12:20:01 -0400

    finalizando a camada de repository do post

--- domain/repository/ComentsRepository.cs
+++ domain/repository/ComentsRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace API_de_rede_social.domain.repository
+﻿using API_de_rede_social.domain.entities;
+using API_de_rede_social.domain.repository.repositories;
+using API_de_rede_social.infraestructure.database;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_de_rede_social.domain.repository
 {
     public class ComentsRepository
     {
+
     }
 }
--- domain/repository/PostRepository.cs
+++ domain/repository/PostRepository.cs
@@ -1,6 +1,64 @@
-﻿namespace API_de_rede_social.domain.repository
+﻿using API_de_rede_social.domain.entities;
+using API_de_rede_social.domain.repository.repositories;
+using API_de_rede_social.infraestructure.database;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_de_rede_social.domain.repository
 {
-    public class PostRepository
+    public class PostRepository : IPostRepository
     {
+        private readonly SocialNetworkDBContext _db;
+
+        public PostRepository(SocialNetworkDBContext db)
+        {
+            _db = db;
+
+        }
+
+        public async Task<PostEntities> GetByIdAsync(Guid postId)
+        {
+            return await _db.PostEntities.AsNoTracking().FirstOrDefaultAsync(x => x.Id == postId);
+        }
+
+        public async Task<IEnumerable<PostEntities>> GetAllAsync()
+        {
+            return await _db.PostEntities.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<PostEntities> AddAsync(PostEntities post)
+        {
+            await _db.PostEntities.AddAsync(post);
+            await _db.SaveChangesAsync();
+            return post;
+        }
+
+        public async Task<PostEntities> UpdateAsync(PostEntities post)
+        {
+            _db.PostEntities.Update(post);
+            await _db.SaveChangesAsync();
+            return post;
+        }
+
+        public async Task DeleteAsync(Guid postId )
+        {
+            var existing = await _db.PostEntities.FirstOrDefaultAsync(x => x.Id == postId);
+
+            if (existing is null)
+                return;
+
+            _db.PostEntities.Remove(existing);
+            await _db.SaveChangesAsync();
+
+        }
+
+
+        public async Task SaveChangesAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+
+
     }
+    
 }
+
