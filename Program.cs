cb05692d30066a77400f6164d7ea6efc3a88d1b0
danielandersonBC96 <dev.danielanderson@gmail.com>
2025-11-21 12:53:43 -0400

    fazendo alguns ajustes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,55 @@
+using API_de_rede_social.application.api.usecase;
+using API_de_rede_social.application.api.usecase.@interface;
+using API_de_rede_social.application.usecases.comments;
+using API_de_rede_social.application.usecases.posts;
+using API_de_rede_social.domain.repository;
+using API_de_rede_social.domain.repository.repositories;
+using API_de_rede_social.infraestructure.database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// -------------------------
+// Database Context
+// -------------------------
+
+builder.Services.AddDbContext<SocialNetworkDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+// -------------------------
+// Repositories
+// -------------------------
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IUserFollowRepository, UserFollowRepository>();
+
+// -------------------------
+// UseCases - POST
+// -------------------------
+
+builder.Services.AddScoped<ICreatePostUseCase, CreatePostUseCase>();
+builder.Services.AddScoped<IDeletPostUseCase, DeletPostUseCase>();
+builder.Services.AddScoped<IGetPostByIdUseCase, IGetPostByIdUseCase>();
+builder.Services.AddScoped<IGetAllPostUseCase,  IGetAllPostUseCase>();
+
+// -------------------------
+// UseCases - COMMENT
+// -------------------------
+builder.Services.AddScoped<IUpdateCommentUseCase, UpdateCommentUseCase>();
+
+// -------------------------
+// UseCases - USER FOLLOW
+// -------------------------
+//builder.Services.AddScoped<IFollowUserUseCase, FollowUserUseCase>();
+//builder.Services.AddScoped<IUnfollowUserUseCase, UnfollowUserUseCase>();
 
 var app = builder.Build();
 
@@ -17,9 +61,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
